75930b278ab05457d75fd42f1391c6f9d48089b6
ItsAlex <me@itsalex.fr>
2026-03-09 22:58:24 +0100

    fix: prevent fail at startup

--- scripts/up.sh
+++ scripts/up.sh
@@ -3,6 +3,9 @@ set -euo pipefail
 
 cd "$(dirname "$0")/../"
 
+mkdir -p ./volumes/homeassistant/config/.storage
+cp ./config/homeassistant/.storage/onboarding ./volumes/homeassistant/config/.storage/onboarding
+
 docker compose up -d --wait
 curl --location --request POST 'http://127.0.0.1:8123/api/onboarding/users' \
     --header 'Content-Type: application/json' \
