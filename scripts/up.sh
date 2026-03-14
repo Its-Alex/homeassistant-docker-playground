451735161dae1e83b143ddb65af5712decdbeaa9
ItsAlex <me@itsalex.fr>
2026-03-14 11:26:58 +0100

    feat: add meross custom_component

--- scripts/up.sh
+++ scripts/up.sh
@@ -6,6 +6,10 @@ cd "$(dirname "$0")/../"
 mkdir -p ./volumes/homeassistant/config/.storage
 cp ./config/homeassistant/.storage/onboarding ./volumes/homeassistant/config/.storage/onboarding
 
+wget -O /tmp/meross.zip https://github.com/albertogeniola/meross-homeassistant/archive/refs/tags/v1.3.12.zip
+unzip -d /tmp/meross /tmp/meross.zip
+mv /tmp/meross/meross-homeassistant-1.3.12/custom_components/meross_cloud ./config/homeassistant/custom_components/meross_cloud
+
 docker compose up -d --wait
 curl --location --request POST 'http://127.0.0.1:8123/api/onboarding/users' \
     --header 'Content-Type: application/json' \
